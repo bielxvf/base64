031fa601845e18a174a4c8a28b64879097715946
Biel Sala Mimó <bielsalamimo@gmail.com>
2025-07-11 13:38:50 +0200

    Add build

--- nob.c
+++ nob.c
@@ -10,6 +10,8 @@
 #define STD_VERSION "-std=c++23"
 #define WARNINGS    "-Wall", "-Wextra"
 
+#define DEPS "BitStream.o"
+
 int main(int argc, char **argv)
 {
     NOB_GO_REBUILD_URSELF(argc, argv);
@@ -21,7 +23,19 @@ int main(int argc, char **argv)
         "c++", 
         STD_VERSION,
         WARNINGS, 
+        "-o", BUILD_FOLDER "BitStream.o", 
+        "-c",
+        SRC_FOLDER"BitStream.cpp");
+
+    if (!nob_cmd_run_sync(cmd)) return 1;
+
+    cmd.count = 0;
+    nob_cmd_append(&cmd,
+        "c++", 
+        STD_VERSION,
+        WARNINGS, 
         "-o", BUILD_FOLDER PROJECT_NAME, 
+        BUILD_FOLDER DEPS,
         SRC_FOLDER"main.cpp");
 
     if (!nob_cmd_run_sync(cmd)) return 1;
