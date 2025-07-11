63eb03e37406bab40947be63266da9def86b078d
Biel Sala Mimó <bielsalamimo@gmail.com>
2025-07-11 11:49:41 +0200

    Nob build system

---
+++ nob.c
@@ -0,0 +1,28 @@
+#define NOB_IMPLEMENTATION
+
+#include "nob.h"
+
+#define BUILD_FOLDER "build/"
+#define SRC_FOLDER   "src/"
+
+#define PROJECT_NAME "base64"
+
+#define STD_VERSION "-std=c++23"
+#define WARNINGS    "-Wall", "-Wextra"
+
+int main(int argc, char **argv)
+{
+    NOB_GO_REBUILD_URSELF(argc, argv);
+
+    if (!nob_mkdir_if_not_exists(BUILD_FOLDER)) return 1;
+
+    Nob_Cmd cmd = {0};
+    nob_cmd_append(&cmd,
+        "c++", 
+        STD_VERSION,
+        WARNINGS, 
+        "-o", BUILD_FOLDER PROJECT_NAME, 
+        SRC_FOLDER"main.cpp");
+
+    if (!nob_cmd_run_sync(cmd)) return 1;
+}
