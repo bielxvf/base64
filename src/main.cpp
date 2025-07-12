9c52cd6a8ad85b6903cec38001719b77d5c08e47
Biel Sala Mimó <bielsalamimo@gmail.com>
2025-07-12 21:08:56 +0200

    Correct encoding now works

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
+#include <vector>
 
 #include "BitStream.hpp"
 
-char alphabet[64] = {
+const char alphabet[64] = {
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
     '+', '/'
 };
+
+bool decode = false;
+std::string output_file;
 
 int main(int argc, char** argv)
 {
@@ -17,19 +21,32 @@ int main(int argc, char** argv)
     while (std::cin.get(ch)) {
         bs.append_byte(ch);
     }
-    /* How could I pad the bitstream before encoding it? */
-    /* Should input be multiple of 24 right? */
-    /* what if we add 0's and just save the "old length" and when we reach past that old length we simply print '='?*/
+    /* pad the input bytes to a multiple of 3, and remember how many actual bytes there were */
+    std::size_t original_size = bs.size() / 8;
+    for (std::size_t i = 0; i < (bs.size() / 8) % 3; i++) {
+        bs.append_byte('\0');
+    }
 
-    /* Group them in four 6-bit groups */
+    std::size_t encoded_size = ((original_size + 2) / 3) * 4;
+    std::vector<char> encoded(encoded_size);
+    std::size_t l = 0;
     for (std::size_t i = 0; i < bs.size(); i += 24) {
         for (std::size_t j = 0; j < 24; j += 6) {
             uint8_t value = 0;
             for (std::size_t k = 0; k < 6; k++) {
                 value <<= 1;
-                value |= bs.get_bit(j + k);
+                value |= bs.get_bit(i + j + k);
             }
-            std::cout << alphabet[value];
+            encoded[l++] = alphabet[value];
         }
+    }
+
+    std::size_t padding_size = bs.size() / 8 - original_size;
+    for (std::size_t i = 0; i < padding_size; i++) {
+        encoded[encoded_size - 1 - i] = '=';
+    }
+
+    for (auto c : encoded) {
+        std::cout << c;
     }
 }
