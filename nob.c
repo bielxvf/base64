cbaade37c6ae4bfa729203177869fc530e655dc9
Biel Sala Mimó <bielsalamimo@gmail.com>
2025-07-12 23:22:24 +0200

    Add build and test subcommands

--- nob.c
+++ nob.c
@@ -23,39 +23,62 @@ int main(int argc, char **argv)
 
     if (!nob_mkdir_if_not_exists(BUILD_DIR)) return 1;
 
-    Nob_Cmd cmd = {0};
-    nob_cmd_append(&cmd,
-        COMPILER, 
-        STD_VERSION,
-        WARNINGS, 
-        "-I" INCLUDE_DIRS,
-        "-o", BUILD_DIR "BitStream.o", 
-        "-c",
-        UTIL_DIR "BitStream.cpp");
-    if (!nob_cmd_run_sync(cmd)) return 1;
+    if (!strcmp(argv[1], "build")) {
+        Nob_Cmd cmd = {0};
+        nob_cmd_append(&cmd,
+            COMPILER, 
+            STD_VERSION,
+            WARNINGS, 
+            "-I" INCLUDE_DIRS,
+            "-o", BUILD_DIR "BitStream.o", 
+            "-c",
+            UTIL_DIR "BitStream.cpp");
+        if (!nob_cmd_run_sync(cmd)) return 1;
 
-    cmd.count = 0;
-    nob_cmd_append(&cmd,
-        COMPILER,
-        STD_VERSION,
-        WARNINGS,
-        "-I" INCLUDE_DIRS,
-        "-o", BUILD_DIR "base64.o",
-        "-c",
-        UTIL_DIR "base64.cpp");
-    if (!nob_cmd_run_sync(cmd)) return 1;
+        cmd.count = 0;
+        nob_cmd_append(&cmd,
+            COMPILER,
+            STD_VERSION,
+            WARNINGS,
+            "-I" INCLUDE_DIRS,
+            "-o", BUILD_DIR "base64.o",
+            "-c",
+            UTIL_DIR "base64.cpp");
+        if (!nob_cmd_run_sync(cmd)) return 1;
 
-    cmd.count = 0;
-    nob_cmd_append(&cmd,
-        COMPILER, 
-        STD_VERSION,
-        WARNINGS, 
-        LIBS,
-        "-I" INCLUDE_DIRS,
-        "-o", BUILD_DIR PROJECT_NAME, 
-        LINKER_DEPS,
-        CORE_DIR "main.cpp");
-    if (!nob_cmd_run_sync(cmd)) return 1;
+        cmd.count = 0;
+        nob_cmd_append(&cmd,
+            COMPILER, 
+            STD_VERSION,
+            WARNINGS, 
+            LIBS,
+            "-I" INCLUDE_DIRS,
+            "-o", BUILD_DIR PROJECT_NAME, 
+            LINKER_DEPS,
+            CORE_DIR "main.cpp");
+        if (!nob_cmd_run_sync(cmd)) return 1;
+    } else if (!strcmp(argv[1], "test")) {
+        Nob_Cmd cmd = {0};
+        nob_cmd_append(&cmd,
+                COMPILER,
+                STD_VERSION,
+                WARNINGS,
+                "-I" INCLUDE_DIRS,
+                LINKER_DEPS,
+                "-o", BUILD_DIR "00_base64",
+                "test/00_base64.cpp");
+        if (!nob_cmd_run_sync(cmd)) return 1;
+
+        nob_log(NOB_INFO, "Running test 00_base64");
+        cmd.count = 0;
+        nob_cmd_append(&cmd,
+                "./" BUILD_DIR "00_base64");
+        if (nob_cmd_run_sync(cmd)) {
+            nob_log(NOB_INFO, "Test 00_base64 \x1b[32mPASSED\x1b[0m");
+        } else {
+            nob_log(NOB_INFO, "Test 00_base64 \x1b[31mFAILED\x1b[0m");
+        }
+    }
 
     return 0;
 }
