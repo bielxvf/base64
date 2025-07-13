8a0fe22ad8cf5d761c045db25c80586550e30eae
Biel Sala Mimó <bielsalamimo@gmail.com>
2025-07-13 15:34:26 +0200

    Automatically build and execute all tests

--- nob.c
+++ nob.c
@@ -1,6 +1,9 @@
 #define NOB_IMPLEMENTATION
 #include "nob.h"
 
+#include <libgen.h>
+#include <dirent.h>
+
 #define COMPILER "c++"
 
 #define BUILD_DIR "build/"
@@ -16,13 +19,22 @@
 
 #define INCLUDE_DIRS "include/"
 
+void strip_extension(const char *path, char *out, size_t out_size)
+{
+    strncpy(out, path, out_size - 1);
+    out[out_size - 1] = '\0';
+
+    char *dot = strrchr(out, '.');
+    if (dot) {
+        *dot = '\0';
+    }
+}
+
 int main(int argc, char **argv)
 {
     NOB_GO_REBUILD_URSELF(argc, argv);
 
     if (!nob_mkdir_if_not_exists(BUILD_DIR)) return 1;
-
-
 
     if (!strcmp(argv[1], "build")) {
         if (!nob_mkdir_if_not_exists(BUILD_DIR "lib")) return 1;
@@ -79,26 +91,54 @@ int main(int argc, char **argv)
             CORE_DIR "main.cpp");
         if (!nob_cmd_run_sync(cmd)) return 1;
     } else if (!strcmp(argv[1], "test")) {
-        /* TODO: Automatically run all tests */
-        Nob_Cmd cmd = {0};
-        nob_cmd_append(&cmd,
-                COMPILER,
-                STD_VERSION,
-                WARNINGS,
-                "-I" INCLUDE_DIRS,
-                LINKER_DEPS,
-                "-o", BUILD_DIR "00_base64",
-                "test/00_base64.cpp");
-        if (!nob_cmd_run_sync(cmd)) return 1;
+        if (!nob_mkdir_if_not_exists(BUILD_DIR "test/")) return 1;
 
-        nob_log(NOB_INFO, "Running test 00_base64");
-        cmd.count = 0;
-        nob_cmd_append(&cmd,
-                "./" BUILD_DIR "00_base64");
-        if (nob_cmd_run_sync(cmd)) {
-            nob_log(NOB_INFO, "Test 00_base64 \x1b[32mPASSED\x1b[0m");
-        } else {
-            nob_log(NOB_INFO, "Test 00_base64 \x1b[31mFAILED\x1b[0m");
+        /* Compile everything under test/ */
+        Nob_Cmd cmd = {0};
+        DIR *dir;
+        struct dirent *ent;
+        if ((dir = opendir("./test")) != NULL) {
+            while ((ent = readdir(dir)) != NULL) {
+                if (!strcmp(ent->d_name, ".") || !strcmp(ent->d_name, "..")) {
+                    continue;
+                }
+                char src_path[2048];
+                sprintf(src_path, "./test/%s", ent->d_name);
+                char dst_path[2048];
+                sprintf(dst_path, BUILD_DIR "test/%s", ent->d_name);
+                strip_extension(dst_path, dst_path, sizeof(dst_path));
+                cmd.count = 0;
+                nob_cmd_append(&cmd,
+                        COMPILER,
+                        STD_VERSION,
+                        WARNINGS,
+                        "-I" INCLUDE_DIRS,
+                        LINKER_DEPS,
+                        "-o", dst_path,
+                        src_path);
+                if (!nob_cmd_run_sync(cmd)) return 1;
+            }
+            closedir(dir);
+        }
+
+        /* Run everything under build/test */
+        if ((dir = opendir(BUILD_DIR "test/")) != NULL) {
+            while ((ent = readdir(dir)) != NULL) {
+                if (!strcmp(ent->d_name, ".") || !strcmp(ent->d_name, "..")) {
+                    continue;
+                }
+                char path[2048];
+                sprintf(path, "./"BUILD_DIR"test/%s", ent->d_name);
+                nob_log(NOB_INFO, "Running test %s", ent->d_name);
+                cmd.count = 0;
+                nob_cmd_append(&cmd,
+                        path);
+                if (nob_cmd_run_sync(cmd)) {
+                    nob_log(NOB_INFO, "Test %s \x1b[32mPASSED\x1b[0m", ent->d_name);
+                } else {
+                    nob_log(NOB_INFO, "Test %s \x1b[31mFAILED\x1b[0m", ent->d_name);
+                }
+            }
         }
     }
 
